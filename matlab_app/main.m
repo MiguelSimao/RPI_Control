5e2342df384f04cb1f2f7b4953d7c80f06786ac6
Miguel Simão <masimao@sapo.pt>
2018-10-15 15:57:11 +0100

    added matlab control app

---
+++ matlab_app/main.m
@@ -0,0 +1,182 @@
+function varargout = main(varargin)
+% MAIN MATLAB code for main.fig
+%      MAIN, by itself, creates a new MAIN or raises the existing
+%      singleton*.
+%
+%      H = MAIN returns the handle to a new MAIN or the handle to
+%      the existing singleton*.
+%
+%      MAIN('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MAIN.M with the given input arguments.
+%
+%      MAIN('Property','Value',...) creates a new MAIN or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before main_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to main_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help main
+
+% Last Modified by GUIDE v2.5 21-Mar-2018 18:37:47
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @main_OpeningFcn, ...
+                   'gui_OutputFcn',  @main_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before main is made visible.
+function main_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to main (see VARARGIN)
+
+% Choose default command line output for main
+handles.output = hObject;
+
+handles.t = tcpip('192.168.1.197',1080,'Terminator','CR/LF');
+handles.isConnected = false;
+
+updateButtonStatus(handles);
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes main wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+function updateButtonStatus(handles)
+
+if handles.isConnected
+    handles.btnOpen.Enable = 'on';
+    handles.btnClose.Enable = 'on';
+    handles.btnConnect.String = 'Disconnect';
+else
+    handles.btnOpen.Enable = 'off';
+    handles.btnClose.Enable = 'off';
+    handles.btnConnect.String = 'Connect';
+end
+
+% --- Outputs from this function are returned to the command line.
+function varargout = main_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function txtIP_Callback(hObject, eventdata, handles)
+% hObject    handle to txtIP (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txtIP as text
+%        str2double(get(hObject,'String')) returns contents of txtIP as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txtIP_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txtIP (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in btnConnect.
+function btnConnect_Callback(hObject, eventdata, handles)
+% hObject    handle to btnConnect (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+t = handles.t;
+isConnected = handles.isConnected;
+
+if ~isConnected
+    % Update remote IP address
+    t.RemoteHost = handles.txtIP.String;
+    t.RemotePort = 1080;
+
+
+    try
+        fopen(t);
+    catch 
+        errordlg('Could not connect to device.');
+        return;
+    end
+
+    handles.isConnected = true;
+    handles.btnConnect.String = 'Disconnect';
+else
+    
+    fclose(t);
+    handles.isConnected = false;
+    handles.btnConnect.String = 'Connect';
+end
+
+updateButtonStatus(handles);
+
+% Update handles structure
+guidata(hObject, handles);
+
+
+% --- Executes on button press in btnOpen.
+function btnOpen_Callback(hObject, eventdata, handles)
+% hObject    handle to btnOpen (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+t = handles.t;
+fprintf(t,'O11');
+
+
+% --- Executes on button press in btnClose.
+function btnClose_Callback(hObject, eventdata, handles)
+% hObject    handle to btnClose (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+t = handles.t;
+fprintf(t,'O10');
+
+
+% --- Executes when user attempts to close figure1.
+function figure1_CloseRequestFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+if handles.isConnected
+    fclose(handles.t); end
+
+% Hint: delete(hObject) closes the figure
+delete(hObject);
